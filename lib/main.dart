21b91c5e345c8dee0d6927084df11efd34548e85
Akshay Mohan <akshmo.95@gmail.com>
2023-10-22 14:56:39 +0530

    Added home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:bloc_ecommerce/Features/Home/UI/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(new MyApp());
+}
 
-//Hello?
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+K
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
 
-//YAAAAAAAAh
 
-//WTFFFFFFFF
